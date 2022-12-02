88bac1bdbe8a452ce7e8d516fbdda37518b0a926
Ruebled <dan.canevschii@gmail.com>
2022-12-02 21:26:18 +0200

    added check function definition file

---
+++ check.h
@@ -0,0 +1,12 @@
+#ifndef CHECK_H
+#define CHECK_H
+
+int check_input_validity(char*);
+
+int check_ip(char*);
+
+int check_port(char*);
+
+int check_local_filename(char*);
+
+#endif
--- client.c
+++ client.c
@@ -4,113 +4,67 @@
 #include <string.h>
 
 #include "trim.h"
-#include "socketfunc.h"
+#include "check.h"
 
 //catch and disable function
 //of signal SIGINT
 //produced by CTRL+C
-static volatile sig_atomic_t keep_running = 1;
+static volatile sig_atomic_t _;
 
 static void sig_handler()
 {
+
 	printf("\b\b  \n");
 	printf("ftp--> ");
 	fflush(stdout);
 }
-
-
-
-//start main
 //
 
 int main(int argc, char *argv[])
 {
+	(void)_;
 
-	//catch CTRL+C command and make it useless
+	//"disable CTRL+C signal aka SIGINT" function call
 	signal(SIGINT, sig_handler);
 
-	//string for taking console input aka. commands
-	char *str;
+	//char * variable for storing user input
+	char *command;
+	command = (char*)malloc(1000*sizeof(char));
 
-	//don't forget to free()...
-	str = (char*)malloc(100*sizeof(char));
+	//char* variable for stroring server response
+	char *reply;
+	reply = (char*)malloc(1000*sizeof(char));
+
 	
-	
-	if (argc == 2) 
+	while(1)
 	{
-		if (!strcmp(argv[1],"--help"))
-		{
-			printf(
-					"Syntax:\n"
-					"open ip [port]\n"
-					"send file_name\n"
-				  );
-		}
-	}
-	
-	while(keep_running)
-	{
+		//user interface 
+		//get string input from stdin
 		printf("ftp--> ");
-		fgets(str, 100, stdin);
-		str = trim(str);
-		
-		const char *ch = "";
+		fgets(command, 1000, stdin);
+		command = trim(command);
 
-		
-		
-		if (str != ch)
+		if(check_input_validity(command))
 		{
-			int res = try_execute(str);			
-			switch (res)
+			if(status.is_conected)
 			{
-				case -1:
-					if(status_connection(clientfd))
-					{
-						if(close_connection(clientfd)<0)
-						{
-							printf("Connection closed\n");
-						}
-					}
-					printf("Bye\n");
-					exit(0);
-					break;
-				case 0:
-					//command invalid
-					printf("?Invalid Command\n");
-					break;
+				ftp_execute(command);
+				char *res = ftp_response(status.socketaddr);
+				process_res(res);
 			}
-
+			else
+			{
+				printf("Not connected\n");
+			}
 		}
-
-		//check res output and print for any 
-		//error or success in stdout
+		else
+		{
+			printf("Command Not Valid\n");
+		}
 		
 	}
-	
-	//
-	////assigning necesarry data for connection
-	//server.sin_addr.s_addr = inet_addr("192.168.0.106");
 
-	//if (connect(clientfd, (struct sockaddr *)&server, sizeof(server))<0){
-	//	printf("connect error");
-	//	return 1;
-	//}
-	//puts("Connected\n");
-
-	//char server_reply[2000];
-	//char *message;
-	//char *pass;
-
-	//message = "USER android\n";
-	//pass = "android";
-	////recv wait for response from server if less that 0 [-1] then operation failed
-	//recv(clientfd, server_reply, 2000, 0);
-	//puts(server_reply);
-	////send function sends a message to server, returns -1 if failed, and more than or equal 0 if succeded
-	//send(clientfd, message, strlen(message), 0);
-
-
+	free(command);
+	free(reply);
 	return 0;	
 }
-
-
